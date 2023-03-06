a20ba4427b6dacfcb23613edd9e84b0572d77032
JosphineJob <34701912+josphinejob@users.noreply.github.com>
2023-03-06 22:05:01 +1100

    Add pages

--- app/src/androidTest/java/com/example/espresso_loginapp/ui/login/pages/LoginPage.java
+++ app/src/androidTest/java/com/example/espresso_loginapp/ui/login/pages/LoginPage.java
@@ -1,4 +1,51 @@
 package com.example.espresso_loginapp.ui.login.pages;
 
+import com.example.espresso_loginapp.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 public class LoginPage {
+    private static final String loginPageTitle = "Espresso-LoginApp";
+    private static final String validEmail = "user" + System.currentTimeMillis() + "@test.com";
+    int loginButtonId = R.id.login;
+
+    public LoginPage verifyLoginPageTitleDisplayed() {
+        onView(withText(loginPageTitle)).check(matches(isDisplayed()));
+        return this;
+    }
+
+    public LoginPage verifyUserNameFieldDisplayed() {
+        onView(withId(R.id.username)).check(matches(isDisplayed()));
+        return this;
+    }
+
+    public LoginPage verifyPasswordFieldDisplayed() {
+        onView(withId(R.id.password)).check(matches(isDisplayed()));
+        onView(withId(loginButtonId)).check(matches(isDisplayed()));
+        return this;
+    }
+
+    public LoginPage verifyLoginButtonDisplayed() {
+        onView(withId(loginButtonId)).check(matches(isDisplayed()));
+        return this;
+    }
+
+    public void typeValidEmail() {
+        onView(withId(R.id.username)).perform(typeText(validEmail));
+    }
+
+    public void typePassword() {
+        onView(withId(R.id.password)).perform(typeText("password"), closeSoftKeyboard());
+    }
+
+    public void tapLoginButton() {
+        onView(withId(loginButtonId)).perform(click());
+    }
 }
--- app/src/androidTest/java/com/example/espresso_loginapp/ui/login/tests/LoginPageTest.java
+++ app/src/androidTest/java/com/example/espresso_loginapp/ui/login/tests/LoginPageTest.java
@@ -6,28 +6,18 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.filters.SmallTest;
 
-import com.example.espresso_loginapp.R;
 import com.example.espresso_loginapp.ui.login.LoginActivity;
+import com.example.espresso_loginapp.ui.login.pages.LoginPage;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isNotEnabled;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4.class)
 public class LoginPageTest {
-    private static final String loginPageTitle = "Espresso-LoginApp";
-    private static final String validEmail = "user" + System.currentTimeMillis() + "@test.com";
-    int loginButtonId = R.id.login;
+    LoginPage loginPage = new LoginPage();
 
     @Rule
     public ActivityScenarioRule<LoginActivity> activityTestRule = new ActivityScenarioRule<>(LoginActivity.class);
@@ -35,18 +25,18 @@ public class LoginPageTest {
     @Test
     @SmallTest
     public void verifyLoginPageUI() {
-        onView(withText(loginPageTitle)).check(matches(isDisplayed()));
-        onView(withId(R.id.username)).check(matches(isDisplayed()));
-        onView(withId(R.id.password)).check(matches(isDisplayed()));
-        onView(withId(loginButtonId)).check(matches(isDisplayed()));
+        loginPage.verifyLoginPageTitleDisplayed()
+                .verifyUserNameFieldDisplayed()
+                .verifyPasswordFieldDisplayed()
+                .verifyLoginButtonDisplayed();
     }
 
     @Test
     @LargeTest
     public void verifyValidLogin() {
-        onView(withText(loginPageTitle)).check(matches(isDisplayed()));
-        onView(withId(R.id.username)).perform(typeText(validEmail));
-        onView(withId(R.id.password)).perform(typeText("password"), closeSoftKeyboard());
-        onView(withId(loginButtonId)).perform(click());
+        loginPage.verifyLoginPageTitleDisplayed();
+        loginPage.typeValidEmail();
+        loginPage.typePassword();
+        loginPage.tapLoginButton();
     }
 }
